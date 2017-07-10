633c9b268ba0863931b729530fd92f981a8efe85
Diljeet <jeetds1994@gmail.com>
2017-07-10 10:50:11 -0400

    submit

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,20 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new
+    @guest.name = params[:guest][:name]
+    @guest.occupation = params[:guest][:occupation]
+    @guest.save
+    redirect_to @guest
+  end
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,8 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-
+# 
+# use GuestsController
+# use SessionsController
+# use EpisodesController
 run Rails.application
